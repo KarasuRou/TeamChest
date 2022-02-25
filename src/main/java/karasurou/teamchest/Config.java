80a275ddf50929a19ef677973a2368e1dc25900d
KarasuRou <Tjalf0000@gmx.de>
2022-02-25 13:48:04 +0100

    Adding Configuration class and files

--- src/main/java/karasurou/teamchest/Config.java
+++ src/main/java/karasurou/teamchest/Config.java
@@ -1,11 +1,29 @@
 package karasurou.teamchest;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+
 public class Config {
+
+    private static Plugin plugin;
+    private static FileConfiguration configuration;
+    private final static String configurationFile = "config.yml";
+    private static YamlConfiguration language;
+    private static String languageFile = "";
+
+    private Config() {}
     
-    
-    public static void loadConfig() {
+    public static void loadConfig(Plugin plugin) {// TODO: 25.02.2022  
+        Config.plugin = plugin;
+        configuration = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), configurationFile));
+        languageFile = configuration.getString("language-file-"+System.getProperty("user.language"),"language.yml");
+        language = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), configurationFile));
     }
 
-    public static void unloadConfig() {
+    public static void unloadConfig() {// TODO: 25.02.2022  
     }
+
 }
