c4edfb48d0328105874ee847c9096f19381359a8
KarasuRou <Tjalf0000@gmx.de>
2022-02-25 13:49:08 +0100

    Rough integration of Configuration

--- src/main/java/karasurou/teamchest/TeamChest.java
+++ src/main/java/karasurou/teamchest/TeamChest.java
@@ -2,11 +2,13 @@ package karasurou.teamchest;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+// TODO: 25.02.2022  
 /**
  * <h3>MINECRAFT SERVER PLUGIN</h3>
- * This plugin will ...<br/>
+ * This plugin will add a new feature ...<br/>
  * <br/>
  * Permissions if needed:<br/>
  * teamchest.com - des ({@code command})<br/>
@@ -15,13 +17,16 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class TeamChest extends JavaPlugin {
 
+    private static Plugin plugin;
+
     @Override
-    public void onEnable() {
-        Config.loadConfig();
+    public void onEnable() {// TODO: 25.02.2022  
+        plugin = this;
+        Config.loadConfig(this);
     }
 
     @Override
-    public void onDisable() {
+    public void onDisable() {// TODO: 25.02.2022  
         Config.unloadConfig();
     }
 
