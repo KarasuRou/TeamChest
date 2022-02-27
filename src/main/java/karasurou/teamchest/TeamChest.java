fd816905d06cf77e779926f0758c108724e63bbb
KarasuRou <Tjalf0000@gmx.de>
2022-02-27 21:24:10 +0100

    Rearranged commands, now with prefix "tc" OR "tech" OR "teamchest"

--- src/main/java/karasurou/teamchest/TeamChest.java
+++ src/main/java/karasurou/teamchest/TeamChest.java
@@ -5,6 +5,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // TODO: 25.02.2022  
 /**
  * <h3>MINECRAFT SERVER PLUGIN</h3>
@@ -23,6 +26,34 @@ public class TeamChest extends JavaPlugin {
     public void onEnable() {// TODO: 25.02.2022
         plugin = this;
         Config.loadConfig(this);
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> commands = new ArrayList<>();
+        if (sender.hasPermission("teamchest.debug")) {
+            commands.add("debug");
+        }
+        if (sender.hasPermission("teamchest.command")) {
+            commands.add("createTeam");
+            commands.add("deleteTeam");
+            commands.add("inviteToTeam");
+            commands.add("acceptTeamInvitation");
+            commands.add("denyTeamInvitation");
+            commands.add("leaveTeam");
+            commands.add("kickFromTeam");
+            commands.add("version");
+        }
+        if (args != null && args.length == 1) {
+            List<String> list = new ArrayList<>();
+            for (String s : commands) {
+                if (s.startsWith(args[0])) {
+                    list.add(s);
+                }
+            }
+            return list;
+        }
+        return commands;
     }
 
     public static Plugin getPlugin(){
