2a2c0de105a88abf1d3cad013ab7a7d88299e456
KarasuRou <Tjalf0000@gmx.de>
2022-03-11 12:03:18 +0100

    No Debug for current release

--- src/main/java/karasurou/teamchest/ChestListener.java
+++ src/main/java/karasurou/teamchest/ChestListener.java
@@ -2,7 +2,6 @@ package karasurou.teamchest;
 
 import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,17 +11,15 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 
-import java.util.Arrays;
-
 public class ChestListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onSignWritten(SignChangeEvent event) {
-        Player p = event.getPlayer();
-        String message = "Lines: " + Arrays.toString(event.getLines())  + "\n" +
-                         "Block: " + event.getBlock() + "\n";
-        event.getBlock().getLocation(); // TODO: 10.03.2022 Location of Sign
-        sendMessage(p, message); // TODO: 10.03.2022 Check sign permission
+//        Player p = event.getPlayer();
+//        String message = "Lines: " + Arrays.toString(event.getLines())  + "\n" +
+//                         "Block: " + event.getBlock() + "\n";
+//        event.getBlock().getLocation(); // TODO: 10.03.2022 Location of Sign
+//        sendMessage(p, message); // TODO: 10.03.2022 Check sign permission
     }
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
@@ -30,7 +27,8 @@ public class ChestListener implements Listener {
         if (TeamChestAPI.isProtectedChest(event.getBlock())) {
             event.setCancelled(true);
         } else if (TeamChestAPI.isProtectionSign(event.getBlock())) {
-            sendMessage(event.getPlayer(), Arrays.toString(((Sign) event.getBlock().getState()).getLines())); // TODO: 10.03.2022 Check sign permission
+            event.setCancelled(true);
+//            sendMessage(event.getPlayer(), Arrays.toString(((Sign) event.getBlock().getState()).getLines())); // TODO: 10.03.2022 Check sign permission
 //            Player player = event.getPlayer();
 //            if () // TODO: 10.03.2022 Check if user is allowed to remove the sign
 //            sendMessage(player, Config.getLanguage("no-teamowner"));
